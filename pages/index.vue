eb5de805e62ad3a37bc6e0fce38703a5f39847de
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-01-31 12:19:00 -0600

    Added better positioning of the search form based on www.google.com.

--- pages/index.vue
+++ pages/index.vue
@@ -58,9 +58,13 @@ export default {
     }
 
     main {
+        position: relative;
+        top: -10%;
+
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
 
         height: 100%;
     }
