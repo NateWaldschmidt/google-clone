7466f65e8a1528785285cd9e03ebe60a9f8816f4
Nate.Waldschmidt@bps101.net <nathaniel.waldsch@gmail.com>
2022-01-30 20:27:07 -0600

    Added the header and the app drawer/ container.  It shows and hides properly but I have not gotten the userprofile popup to chage yet.

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,19 @@
 <template>
-    <main>
-        <landing-search />
-        <landing-button type="submit">Google Search</landing-button>
-    </main>
+    <div id="google-clone">
+        <landing-header />
+        <main>
+            <landing-search />
+            <section>
+                <landing-button type="submit">Google Search</landing-button>
+                <landing-button type="submit">I'm Feeling Lucky</landing-button>
+            </section>
+        </main>
+    </div>
 </template>
 
 <script>
 import LandingButton from '../components/landing-button.vue';
+import LandingHeader from '../components/landing-header.vue';
 import LandingSearch from '../components/landing-search.vue';
 
 export default {
@@ -14,7 +21,8 @@ export default {
 
     components: {
         LandingButton,
-        LandingSearch
+        LandingSearch,
+        LandingHeader
     },
 
     data: function() {
@@ -30,6 +38,14 @@ export default {
     }
 }
 </script>
+
+<style>
+    /* Allows easier styling of the header. */
+    body {
+        height: 100vh;
+        margin: 0;
+    }
+</style>
 
 <style scoped>
     main {
